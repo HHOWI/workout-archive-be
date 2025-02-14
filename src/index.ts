5934c2dfaa3c3c4d53b494c01557212d471fde2b
HHOWI <minsu705@gmail.com>
2025-02-15 02:33:41 +0900

    feat:cors 설정

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,18 @@ import "reflect-metadata";
 import { AppDataSource } from "./data-source";
 import userRouter from "./routes/UserRouter";
 import "./jobs/CleanupUnverifiedUsers";
-import { GlobalErrorHandler } from "./middlewares/globalErrorHandler";
+import { GlobalErrorHandler } from "./middlewares/GlobalErrorHandler";
+import cors from "cors";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:3000", // 프론트엔드 주소 (포트 3000)
+    credentials: true, // 쿠키 등 인증 정보가 필요한 경우 true
+  })
+);
+
 app.use(express.json());
 
 // 라우터 등록
