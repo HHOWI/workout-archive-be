89c60f97d4613d69d673f90ae806f7122ae218c2
HHOWI <minsu705@gmail.com>
2025-02-14 09:21:58 +0900

    feat:포트주소 변경

--- src/index.ts
+++ src/index.ts
@@ -17,8 +17,8 @@ app.use(GlobalErrorHandler);
 AppDataSource.initialize()
   .then(() => {
     console.log("Database connected successfully!");
-    app.listen(3000, () => {
-      console.log("Server running on http://localhost:3000");
+    app.listen(process.env.PORT, () => {
+      console.log("Server running on http://localhost:" + process.env.PORT);
     });
   })
   .catch((error) => {
